62f171841dbfc2039fac5d682c4030f44097c9eb
Ishanka D Senevirathne <ishankadsenevirathne@gmail.com>
2022-06-14 14:00:42 +0530

    ignoring eslint errors

--- next.config.js
+++ next.config.js
@@ -8,4 +8,9 @@ module.exports = {
     domains:['res.cloudinary.com','lh3.googleusercontent.com']
   },
   nextConfig:nextConfig,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
