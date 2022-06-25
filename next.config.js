23ebecb8857300bd402bef338a8219272b2c0740
Ishanka D Senevirathne <33729848+IshankaDSenevirathne@users.noreply.github.com>
2022-06-26 00:21:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,9 +19,4 @@ module.exports = withPWA({
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  env:{  
-    STRIPE_PUBLIC_KEY : "pk_test_51KxW1JEo5iGGoetegazbVVAXP2Y5M0LpnHOgqEzIa7IVZd2wOGJlQGZsNf1INTekkVoj10PfIlIlWy8kjS1xDiKx00aAPcoWhB",
-    STRIPE_SECRET_KEY:"sk_test_51KxW1JEo5iGGoeteVt25jzUJwwae5p2RwOFOkdvEamvOJV45Uoub4oU8w6r3TBdv25nvpzsCHgPOe1GMfXe7zMql00mKBQngNg",
-    STRIPE_WEBHOOK_SECRET:"whsec_7e73f7c93058c3b58c10bb85623d24e211c13e6223319568f9dcbdd273c90853"
-  }
 });
